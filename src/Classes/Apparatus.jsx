2223fb01abfedc53749693b696fd6a67000848e5
maxxer789 <maxvdd76@gmail.com>
2021-01-12 15:10:18 +0100

    better layout

--- src/Classes/Apparatus.jsx
+++ src/Classes/Apparatus.jsx
@@ -4,6 +4,7 @@ const Apparatus = props => {
     return (
         <button onClick={props.inputHandler} className="apparatusButton" name="apparatusId" value={props.apparatus.id}>
                     {props.apparatus.name}
+                    <br />
                     {props.apparatus.abbreviation}
         </button>
     )
